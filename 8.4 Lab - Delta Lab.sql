f758c6fdf8c5558056d1c1432b6a1e2eb176be85
Jonatan Polanco <65262178+JonatanPolanco@users.noreply.github.com>
2022-03-17 14:26:43 -0500

    Add files via upload

---
+++ 8.4 Lab - Delta Lab.sql
@@ -0,0 +1,341 @@
+-- Databricks notebook source
+-- MAGIC 
+-- MAGIC %md-sandbox
+-- MAGIC 
+-- MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+-- MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
+-- MAGIC </div>
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC # Lab 4 - Delta Lab
+-- MAGIC ## Module 8 Assignment
+-- MAGIC In this lab, you will continue your work on behalf of Moovio, the fitness tracker company. You will be working with a new set of files that you must move into a "gold-level" table. You will need to modify and repair records, create new columns, and merge late-arriving data. 
+
+-- COMMAND ----------
+
+-- MAGIC %run ../Includes/Classroom-Setup
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 1: Create a table
+-- MAGIC 
+-- MAGIC **Summary:** Create a table from `json` files. 
+-- MAGIC 
+-- MAGIC Use this path to access the data: <br>
+-- MAGIC `"dbfs:/mnt/training/healthcare/tracker/raw.json/"`
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a table named `health_tracker_data_2020`
+-- MAGIC * Use optional fields to indicate the path you're reading from and epress that the schema should be inferred. 
+
+-- COMMAND ----------
+
+-- TODO
+DROP TABLE IF EXISTS health_tracker_data_2020;
+CREATE TABLE health_tracker_data_2020
+USING json
+OPTIONS(
+PATH "dbfs:/mnt/training/healthcare/tracker/raw.json/",
+INFERSCHEMA "true"
+)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 2: Preview the data
+-- MAGIC 
+-- MAGIC **Summary:**  View a sample of the data in the table. 
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Query the table with `SELECT *` to see all columns
+-- MAGIC * Sample 5 rows from the table
+
+-- COMMAND ----------
+
+-- TODO
+SELECT * FROM health_tracker_data_2020
+LIMIT 5
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 3: Count Records
+-- MAGIC **Summary:** Write a query to find the total number of records
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Count the number of records in the table
+-- MAGIC 
+-- MAGIC **Answer the corresponding question in Coursera**
+
+-- COMMAND ----------
+
+-- TODO
+SELECT COUNT(*) FROM health_tracker_data_2020
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 4: Create a Silver Delta table
+-- MAGIC **Summary:** Create a Delta table that transforms and restructures your table
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Drop the existing `month` column
+-- MAGIC * Isolate each property of the object in the `value` column to its own column
+-- MAGIC * Cast time as timestamp **and** as a date
+-- MAGIC * Partition by `device_id`
+-- MAGIC * Use Delta to write the table
+
+-- COMMAND ----------
+
+-- TODO
+DROP TABLE IF EXISTS SILVER_TABLE;
+CREATE TABLE SILVER_TABLE
+USING DELTA
+PARTITIONED BY (device_id)
+LOCATION "dbfs:/health_tracker/silver" AS(
+SELECT 
+  value.device_id,
+  value.name,
+  value.heartrate,
+  CAST(FROM_UNIXTIME(value.time) AS timestamp) AS time,
+  CAST(FROM_UNIXTIME(VALUE.TIME) AS date) AS dates
+  FROM health_tracker_data_2020
+)
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 5: Register table to the metastore
+-- MAGIC **Summary:** Register your Silver table to the Metastore
+-- MAGIC Steps to complete: 
+-- MAGIC * Be sure you can run the cell more than once without throwing an error
+-- MAGIC * Write to the location: `/health_tracker/silver`
+
+-- COMMAND ----------
+
+-- TODO
+DROP TABLE IF EXISTS SILVER_TABLE;
+CREATE TABLE SILVER_TABLE
+USING DELTA
+LOCATION "/health_tracker/silver"
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 6: Check the number of records
+-- MAGIC **Summary:** Check to see if all devices are reporting the same number of records
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Write a query that counts the number of records for each device
+-- MAGIC * Include your partitioned device id column and the count of those records
+-- MAGIC 
+-- MAGIC **Answer the corresponding question in Coursera**
+
+-- COMMAND ----------
+
+--TODO
+SELECT COUNT(*), device_id
+FROM SILVER_TABLE
+GROUP BY device_id
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 7: Plot records
+-- MAGIC **Summary:** Attempt to visually assess which dates may be missing records
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Write a query that will return records from one devices that is **not** missing records as well as the device that seems to be missing records
+-- MAGIC * Plot the results to visually inspect the data
+-- MAGIC * Identify dates that are missing records
+-- MAGIC 
+-- MAGIC **Answer the corresponding question in Coursera**
+
+-- COMMAND ----------
+
+--TODO
+SELECT * FROM SILVER_TABLE
+WHERE device_id IN (0,4)
+ORDER BY dates
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 8: Check for Broken Readings
+-- MAGIC **Summary:** Check to see if your data contains records that would indicate a device has misreported data
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a view that contains all records reporting a negative heartrate
+-- MAGIC * Plot/view that data to see which days include broken readings
+
+-- COMMAND ----------
+
+--TODO
+CREATE OR REPLACE TEMPORARY VIEW broken_readings
+AS (
+  SELECT COUNT(*) AS count_broken_readings, dates
+  FROM SILVER_TABLE
+  WHERE heartrate < 0
+  GROUP BY dates
+  ORDER BY dates);
+  
+SELECT * FROM broken_readings
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 9: Repair records
+-- MAGIC **Summary:** Create a view that contains interpolated values for broken readings
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a temporary view that will hold all the records you want to update. 
+-- MAGIC * Transform the data such that all broken readings (where heartrate is reported as less than zero) are interpolated as the mean of the the data points immediately surrounding the broken reading. 
+-- MAGIC * After you write the view, count the number of records in it. 
+-- MAGIC 
+-- MAGIC **Answer the corresponding question in Coursera** 
+
+-- COMMAND ----------
+
+--TODO
+CREATE OR REPLACE TEMPORARY VIEW updates
+AS (
+  SELECT  
+  name,
+  (prev + next)/2 AS heartrate,
+  time, 
+  dates,
+  device_id
+  FROM (
+    SELECT *,
+      LAG(heartrate) OVER (PARTITION BY device_id, dates ORDER BY device_id, dates) AS prev,
+      LEAD(heartrate) OVER (PARTITION BY device_id, dates ORDER BY device_id, dates) AS next
+    FROM SILVER_TABLE
+    )
+WHERE heartrate < 0
+);
+
+SELECT COUNT(*) FROM updates
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 10: Read late-arriving data
+-- MAGIC **Summary:** Read in new late-arriving data
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a new table that contains the late arriving data at this path: `"dbfs:/mnt/training/healthcare/tracker/raw-late.json"`
+-- MAGIC * Count the records <br/>
+-- MAGIC 
+-- MAGIC **Answer the corresponding question in Coursera**
+
+-- COMMAND ----------
+
+--TODO
+DROP TABLE IF EXISTS health_tracker_late;
+CREATE TABLE health_tracker_late
+USING json
+OPTIONS ( path "dbfs:/mnt/training/healthcare/tracker/raw-late.json",
+inferSchema "true");
+
+SELECT COUNT(*) FROM health_tracker_late
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 11: Prepare inserts
+-- MAGIC **Summary:** Prepare your new, late-arriving data for insertion into the Silver table
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a temporary view that holds the new late-arriving data
+-- MAGIC * Apply transformations to the data so that the schema matches our existing Silver table
+
+-- COMMAND ----------
+
+--TODO
+CREATE OR REPLACE TEMPORARY VIEW inserts
+AS (
+  SELECT
+    value.name,
+    value.heartrate,
+    CAST(FROM_UNIXTIME(value.time) AS timestamp) AS time,
+    CAST(FROM_UNIXTIME(value.time) AS DATE) AS dates,
+    value.device_id
+  FROM health_tracker_late
+  )
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 12: Prepare upserts
+-- MAGIC **Summary:** Prepare a view to upsert to our Silver table
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a temporary view that is the `UNION` of the views that hold data you want to insert and data you want to update
+-- MAGIC * Count the records
+-- MAGIC 
+-- MAGIC **Answer the corresponding question in Coursera**
+
+-- COMMAND ----------
+
+--TODO
+CREATE OR REPLACE TEMPORARY VIEW upserts
+AS (
+    SELECT * FROM updates
+    UNION ALL
+    SELECT * FROM inserts
+);
+
+
+SELECT COUNT(*) FROM upserts
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 13: Perform upserts
+-- MAGIC 
+-- MAGIC **Summary:** Merge the upserts into your Silver table
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Merge data on the time and device id columns from your Silver table and your upserts table
+-- MAGIC * Use `MATCH`conditions to decide whether to apply an update or an insert
+
+-- COMMAND ----------
+
+--TODO
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Exercise 14: Write to gold
+-- MAGIC **Summary:** Create a Gold level table that holds aggregated data
+-- MAGIC 
+-- MAGIC Steps to complete: 
+-- MAGIC * Create a Gold-level Delta table
+-- MAGIC * Aggregate heartrate to display the average and standard deviation for each device. 
+-- MAGIC * Count the number of records
+
+-- COMMAND ----------
+
+--TODO
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Cleanup
+-- MAGIC Run the following cell to clean up your workspace. 
+
+-- COMMAND ----------
+
+-- %run .Includes/Classroom-Cleanup
+
+
+-- COMMAND ----------
+
+-- MAGIC %md-sandbox
+-- MAGIC &copy; 2020 Databricks, Inc. All rights reserved.<br/>
+-- MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
+-- MAGIC <br/>
+-- MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="http://help.databricks.com/">Support</a>
